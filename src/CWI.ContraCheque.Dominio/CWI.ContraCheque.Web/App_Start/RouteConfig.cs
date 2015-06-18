4a06a26a74b392049d3fc592e4d0b45a68ec558a
juarezmonteiro <jrzmonteiro@gmail.com>
2015-06-18 14:39:18 -0300

    Fix(route config)

--- src/CWI.ContraCheque.Dominio/CWI.ContraCheque.Web/App_Start/RouteConfig.cs
+++ src/CWI.ContraCheque.Dominio/CWI.ContraCheque.Web/App_Start/RouteConfig.cs
@@ -23,7 +23,7 @@ namespace CWI.ContraCheque.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Account", action = "Login2", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
