0a42c01b91742d93bb4f6a3f4ad49847c61675d6
Giordano Trombetta <gior.rs@hotmail.com>
2015-06-18 18:33:27 -0300

    if do usuário

--- src/CWI.ContraCheque.Dominio/CWI.ContraCheque.Web/Controllers/ContraChequeController.cs
+++ src/CWI.ContraCheque.Dominio/CWI.ContraCheque.Web/Controllers/ContraChequeController.cs
@@ -44,8 +44,12 @@ namespace CWI.ContraCheque.Web.Controllers
         private bool VerificaUsuarioComum()
         {
             var userId = User.Identity.GetUserId();
-            var user = UserManager.FindById(userId);
-            return user.Roles.Any(x => x.RoleId == "2");
+            if (userId != null)
+            {
+                var user = UserManager.FindById(userId);
+                return user.Roles.Any(x => x.RoleId == "2");
+            }
+            return false;
         }
 
         public ActionResult Index()
--- src/CWI.ContraCheque.Dominio/CWI.ContraCheque.Web/Controllers/ImportacaoContraChequeController.cs
+++ src/CWI.ContraCheque.Dominio/CWI.ContraCheque.Web/Controllers/ImportacaoContraChequeController.cs
@@ -44,10 +44,14 @@ namespace CWI.ContraCheque.Web.Controllers
         }
 
         private bool VerificaUsuarioAdmin()
-        {
+        {            
             var userId = User.Identity.GetUserId();
-            var user = UserManager.FindById(userId);
-            return user.Roles.Any(x => x.RoleId == "1");
+            if (userId != null)
+            {
+                var user = UserManager.FindById(userId);
+                return user.Roles.Any(x => x.RoleId == "1");
+            }
+            return false;
         }
 
 
